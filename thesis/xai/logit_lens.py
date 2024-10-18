aef82a6abe70669f4ba76e337eedf07edf0970f6
MartinPreiss <martinpreiss524@gmail.com>
2024-10-18 16:43:33 +0200

    logit_lens draft finished

--- thesis/xai/logit_lens.py
+++ thesis/xai/logit_lens.py
@@ -4,7 +4,7 @@ from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
 )
-from thesis.utils import get_prompt_df, print_cuda_info,clear_unused_gpu_memory, write_pickle
+from thesis.utils import get_prompt_df, print_cuda_info,clear_unused_gpu_memory, clear_all__gpu_memory,write_pickle
 
 from thesis.xai.hook import Hook
 from tqdm import tqdm
@@ -12,14 +12,22 @@ from tqdm import tqdm
 
 def logit_lens(model, hooks):
     layer_outputs = []
-    for hook in hooks:
-        layer_outputs.append(model.lm_head(hook.data).cpu())
+    with torch.no_grad():
+        for hook in hooks:
+            layer_output = model.lm_head(model.model.norm(hook.data)) #logit lens
+            hook.clear_hook()
+            layer_output = layer_output[:,-1,:] # postprocess (hook only saves last token )
+            #layer_output = torch.nn.functional.softmax(layer_output,dim=1)
+            layer_outputs.append(layer_output)  
     return layer_outputs
 
-def get_hooks(model):
+def get_hooks(model,layer_ids=None):
+    if not layer_ids:
+        layer_ids = range(len(model.model.layers))
     hooks = []
-    for layer in model.model.layers: 
-        hooks.append(Hook(layer))
+    for id,layer in enumerate(model.model.layers): 
+        if id in layer_ids:
+            hooks.append(Hook(layer))
 
     return hooks
 
@@ -29,9 +37,9 @@ def get_label_ids(tokenizer):
 
     # Tokenize and get token IDs for each word in the list
     true_ids = [tokenizer.encode(word, add_special_tokens=False) for word in true_words]
-    true_ids = torch.tensor(true_ids).flatten().cpu()
+    true_ids = torch.tensor(true_ids).flatten().to('cuda')#.cpu()
     false_ids = [tokenizer.encode(word, add_special_tokens=False) for word in false_words]
-    false_ids = torch.tensor(false_ids).flatten().cpu()
+    false_ids = torch.tensor(false_ids).flatten().to('cuda')#.cpu()
     
     return true_ids, false_ids
 
@@ -45,21 +53,18 @@ def save_layer_outputs(prompts,model,tokenizer, hooks,output_path):
     topk_scores = []
     
     for index, prompt in tqdm(prompts.items()):
-        results = {}
         input_ids = tokenizer(prompt, return_tensors="pt").to('cuda')
         #forward pass and final output
-        output = model.generate(**input_ids,max_length=len(input_ids[0])+1, return_dict_in_generate=True, output_scores=True,temperature=0.1,)
+        with torch.no_grad():
+            output = model.generate(**input_ids,max_length=len(input_ids[0])+1, return_dict_in_generate=True, output_scores=True,temperature=0.1)
 
         # Convert the predicted token IDs to text
-        decoded_text = tokenizer.decode(output["sequences"][0][len(input_ids[0]):])
+        decoded_text = tokenizer.decode(output["sequences"][0][-1])
 
         original_answers.append(decoded_text)
         
         #calculate logit_lens / get intermediate scores
-        layer_scores = torch.stack(logit_lens(model,hooks))
-        layer_scores = layer_scores.cpu()
-        clear_unused_gpu_memory
-        
+        layer_scores = logit_lens(model,hooks)
         #calculate metric 
         layer_labeled_true_scores = []
         layer_labeled_false_scores = []
@@ -67,12 +72,12 @@ def save_layer_outputs(prompts,model,tokenizer, hooks,output_path):
         layer_topk_tokens = []
         
         for id, layer_output in enumerate(layer_scores):
-            next_token_output = layer_output[:,-1,:]
-            labeled_true_score = torch.sum(torch.index_select(next_token_output,dim=-1,index=true_ids)).reshape(1)
-            labeled_false_score = torch.sum(torch.index_select(next_token_output,dim=-1,index=false_ids)).reshape(1)
+            #layer_output = torch.nn.functional.relu(layer_output) #only add positive 
+            labeled_true_score = torch.sum(torch.index_select(layer_output,dim=-1,index=true_ids)).reshape(1)
+            labeled_false_score = torch.sum(torch.index_select(layer_output,dim=-1,index=false_ids)).reshape(1)
             #print(labeled_true_score.item()-labeled_false_score.item())
 
-            topk = next_token_output.topk(10,dim=-1)
+            topk = layer_output.topk(10,dim=-1)
             topk_layer_ids = topk[1]
             topk_score = topk[0]
             topk_token = tokenizer.decode(topk_layer_ids[0][-1])
@@ -82,10 +87,16 @@ def save_layer_outputs(prompts,model,tokenizer, hooks,output_path):
             layer_topk_scores.append(topk_score)
             layer_topk_tokens.append(topk_token)
         
-        labeled_true_scores.append(torch.cat(layer_labeled_true_scores))
-        labeled_false_scores.append(torch.cat(layer_labeled_false_scores))
-        topk_scores.append(torch.stack(layer_topk_scores))
+        labeled_true_scores.append(torch.cat(layer_labeled_true_scores).cpu())
+        labeled_false_scores.append(torch.cat(layer_labeled_false_scores).cpu())
+        topk_scores.append(torch.stack(layer_topk_scores).cpu())
         topk_tokens.append(layer_topk_tokens)
+        
+        del input_ids, output, layer_scores#
+        clear_all__gpu_memory()
+        
+
+        #print(torch.cuda.memory_summary())
 
     write_pickle(original_answers,filename="original_answers",path=output_path)
     torch.save(torch.stack(labeled_true_scores),output_path + "labeled_true_scores" + ".pt")
@@ -93,7 +104,6 @@ def save_layer_outputs(prompts,model,tokenizer, hooks,output_path):
     write_pickle(topk_tokens,filename="topk_tokens",path=output_path)
     torch.save(torch.stack(topk_scores),output_path + "topk_scores" + ".pt")
 
-    
 
 if __name__ == "__main__":    
     
@@ -101,20 +111,19 @@ if __name__ == "__main__":
     
     df = get_prompt_df()
     
-    print(len(df))
-    
     df_original_prompt = df["original_prompt"]
     df_fake_prompt = df["transformed_prompt"]
 
-    model_id = 'meta-llama/Meta-Llama-3-8B-Instruct'
-    #model_id = 'google/gemma-2-2b-it'
+    #model_id = 'meta-llama/Meta-Llama-3-8B-Instruct'
+    model_id = 'google/gemma-2-9b-it'
     tokenizer = AutoTokenizer.from_pretrained(model_id,device_map="auto")
     model = AutoModelForCausalLM.from_pretrained(model_id, output_attentions=True,device_map="auto")
     
-
+    print(model)
+    
+    
     #hook layers
     hooks = get_hooks(model)
-    
+
     save_layer_outputs(df_original_prompt,model,tokenizer,hooks,output_path="/home/knowledgeconflict/home/martin/MasterThesis/data/logit_lens/original/")
     save_layer_outputs(df_fake_prompt,model,tokenizer,hooks,output_path="/home/knowledgeconflict/home/martin/MasterThesis/data/logit_lens/fake/")
-
