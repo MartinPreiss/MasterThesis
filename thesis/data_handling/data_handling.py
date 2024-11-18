84e889d658febf854ae72a496166f5ee1bba39a3
MartinPreiss <martinpreiss524@gmail.com>
2024-11-18 15:14:29 +0100

    fixed datasplit leak (hopefully)

--- thesis/data_handling/data_handling.py
+++ thesis/data_handling/data_handling.py
@@ -4,16 +4,55 @@ from torch.utils.data import Dataset,DataLoader, TensorDataset
 from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from thesis.utils import get_device
+from thesis.data_handling.benchmark import get_df
+
 
 def get_embedding_dataset(cfg):
     model_name = cfg.llm.name[cfg.llm.name.rfind("/")+1:]
     dataset = torch.load(f"thesis/data/datasets/embeddings/embedding_{model_name}_{cfg.benchmark.name}.pth")#,map_location=torch.device('cpu'))   
-    if cfg.pca.use_pca:    
+    if cfg.pca.use_pca:
+        raise(Exception("WARNING: PCA not only performed on test set")) 
         dataset = PCADataset(dataset,n_components=cfg.pca.n_components,layer_wise=cfg.pca.layer_wise)
     if  cfg.use_coveig:
         dataset = CovEigDataset(dataset)
         
     return dataset
+
+def get_refact_split(cfg,Y,test_val_size):
+    #get original id
+    df = get_df(cfg)    
+    df["id"] = df.apply(lambda row: row["unique_row_id"][:row["unique_row_id"].find(".")],axis=1)
+    
+    #get_unique_ids
+    grouped = df.groupby("id").indices
+    unique_ids = list(grouped.keys())
+    
+    #problem
+    #len(X)=2002, len(df)=1001
+
+    #perform split on unique_ids
+    train_ids, temp_ids = train_test_split(
+        unique_ids, train_size=test_val_size, random_state=cfg.seed
+    ) 
+    val_ids, test_ids = train_test_split(
+        temp_ids, test_size=0.5, random_state=cfg.seed
+    )
+    
+    # Collect indices for each set based on the `id`s
+    train_indices = [i for id_ in train_ids for i in grouped[id_]]
+    val_indices = [i for id_ in val_ids for i in grouped[id_]]
+    test_indices = [i for id_ in test_ids for i in grouped[id_]]
+    
+    # Return split indices and corresponding `Y` data
+    return (train_indices, val_indices, test_indices)
+
+def print_label_data(y, split_type="total"):
+    
+    num_positive = (y == 1).sum().item()
+    num_negative =  len(y) - num_positive
+    print(f"Percentage of {split_type} positive samples",num_positive / len(y))
+    print(f"Percentage of {split_type} negative samples",num_negative / len(y))
+    
 
 def get_dataloaders(cfg,dataset):
     
@@ -21,20 +60,39 @@ def get_dataloaders(cfg,dataset):
     X = torch.stack([dataset[i][0] for i in range(len(dataset))])
     Y = torch.stack([dataset[i][1] for i in range(len(dataset))])
     
-    #print some statistics 
-    num_positive = (Y == 1).sum().item()
-    num_negative =  len(dataset) - num_positive
-    print("Percentage of positive samples",num_positive / len(dataset))
-    print("Percentage of negative samples",num_negative / len(dataset))
-    
     # Split the dataset
     data_size = len(dataset)
     train_size = int(0.7 * data_size)
     test_val_size = data_size - train_size 
-    x_indices = torch.arange(X.shape[0])
+    x_indices = list(range(X.shape[0]))
     #split indices (sklearn cant handle shapes greater than 3 :) ) 
-    train_indices, X_temp, y_train, y_temp = train_test_split(x_indices, Y, test_size=test_val_size, stratify=Y, random_state=cfg.seed)
-    val_indices, test_indices, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, stratify=y_temp, random_state=cfg.seed)
+    if cfg.benchmark.name == "refact":
+        x_indices = get_refact_split(cfg,Y,test_val_size)
+        train_indices, val_indices, test_indices = x_indices
+    else:
+        #first half
+        len_first_half = len(x_indices)//2
+        indices_first_half = x_indices[:len_first_half]
+        Y_first_half = Y[:len_first_half]
+        train_indices, X_temp, _, y_temp = train_test_split(indices_first_half, Y_first_half, test_size=test_val_size, stratify=Y_first_half, random_state=cfg.seed)
+        val_indices, test_indices, _, _ = train_test_split(X_temp, y_temp, test_size=0.5, stratify=y_temp, random_state=cfg.seed)
+        
+    #get other half
+        #get other half
+    train_indices.extend([id+len_first_half for id in train_indices])
+    val_indices.extend([id+len_first_half for id in val_indices])
+    test_indices.extend([id+len_first_half for id in test_indices])
+    
+    #make tensors 
+    train_indices = torch.tensor(train_indices)
+    val_indices = torch.tensor(val_indices)
+    test_indices = torch.tensor(test_indices)
+    
+    #get y_data
+    y_train = torch.index_select(Y, 0, train_indices)
+    y_val = torch.index_select(Y, 0, val_indices)
+    y_test = torch.index_select(Y, 0, test_indices)
+
 
     #create torch_datasets
     train_dataset = TensorDataset(torch.index_select(X.cpu(), 0, train_indices.cpu()),y_train)
@@ -45,11 +103,16 @@ def get_dataloaders(cfg,dataset):
     train_loader = DataLoader(train_dataset,batch_size=100, shuffle=True)
     val_loader = DataLoader(val_dataset,batch_size=100, shuffle=False)
     test_loader = DataLoader(test_dataset,batch_size=100, shuffle=False)
-        
+    
+    #print some statistics 
     print("Dataset Size", data_size)
     print("Trainset Size", train_size)
     print("Valset Size", len(val_dataset))
     print("Testset Size", len(test_dataset))
+    print_label_data(Y)
+    print_label_data(y_train,"y_train")
+    print_label_data(y_val,"y_val")
+    print_label_data(y_test,"y_test")
     return train_loader, val_loader, test_loader
 
 
