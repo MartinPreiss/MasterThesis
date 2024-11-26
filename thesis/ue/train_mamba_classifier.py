bca5112ba7020a8ec5e573fd40ef17e3d763cb07
MartinPreiss <martinpreiss524@gmail.com>
2024-11-26 11:51:12 +0100

    train mamba with contrastive

--- thesis/ue/train_mamba_classifier.py
+++ thesis/ue/train_mamba_classifier.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 
 from thesis.models.mamba import MambaClassifier
+from thesis.models.loss.contrastive_loss import ContrastiveLoss
 from thesis.metrics import calculate_metrics
 from thesis.data_handling.data_handling import get_embedding_dataset, get_dataloaders
 from thesis.utils import print_number_of_parameters, get_device, init_wandb
@@ -23,7 +24,8 @@ def train(cfg,model, train_loader, val_loader):
 
     num_layers = 42
     # Loss and optimizer
-    criterion = nn.BCEWithLogitsLoss().to(device)
+    classification_loss = nn.BCEWithLogitsLoss().to(device)
+    contrastive_loss = ContrastiveLoss().to(device)
     optimizer = optim.Adam(model.parameters(), lr=cfg.task.training_params.learning_rate,weight_decay=cfg.task.training_params.weight_decay)
     epochs = cfg.task.training_params.epochs
 
@@ -39,13 +41,21 @@ def train(cfg,model, train_loader, val_loader):
             #labels = labels.unsqueeze(1).repeat(1,num_layers,1)
             optimizer.zero_grad()
             # Forward pass
-            outputs = model(inputs)
-            loss = criterion(outputs, labels)
+            outputs, encoded_space = model(inputs,cfg.task.use_contrastive_loss)
+            class_loss = classification_loss(outputs, labels)
+            contrast_loss = 0
+            if cfg.task.use_contrastive_loss:
+                contrast_loss = contrastive_loss(encoded_space,labels)
+                        
+            loss = class_loss + contrast_loss
             # Backward pass and optimization
             loss.backward()
             optimizer.step()
             # Log the loss
             running_loss += loss.item()
+            
+            if cfg.wandb.use_wandb:
+                wandb.log({"classification_loss":class_loss.item(),"contrastive_loss":contrast_loss.item()})
         # validation
         all_preds = []
         all_labels = []
@@ -57,8 +67,12 @@ def train(cfg,model, train_loader, val_loader):
                 inputs, labels = data
                 inputs, labels = inputs.to(device), labels.to(device)
                 #labels = labels.unsqueeze(1).repeat(1,num_layers,1)
-                val_outputs = model(inputs)
-                loss = criterion(val_outputs, labels)
+                val_outputs, encoded_space = model(inputs,cfg.task.use_contrastive_loss)
+                if cfg.task.use_contrastive_loss:
+                    val_loss = classification_loss(val_outputs, labels) + contrastive_loss(encoded_space,labels)
+                else:
+                    val_loss = classification_loss(outputs, labels)
+                    
                 val_loss += loss.item()
                 all_preds.append(val_outputs)
                 all_labels.append(labels)
@@ -105,7 +119,7 @@ def train_mamba_classifier(cfg : DictConfig):
     num_layers = dataset[0][0].shape[-2]  # first batch, first input #embedding size
     print("Embedding Size", input_size, "Number of Layers", num_layers)
 
-    model = MambaClassifier(input_size).to(#,cfg.task.model.num_hidden_layers).to(
+    model = MambaClassifier(input_size,cfg.task.model.num_hidden_layers).to(
         device
     )
 
