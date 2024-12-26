15af0cf79b06f122df88cc115d868863aac5e0b9
MartinPreiss <martinpreiss524@gmail.com>
2024-12-26 12:04:01 +0100

    token_encoder plot last_layer weights attention fusion model ensemble weight sum

--- thesis/models/model_handling.py
+++ thesis/models/model_handling.py
@@ -1,4 +1,4 @@
-from thesis.models.neural_net import AllLayerClassifier, LayerFusion, MLP, LayerFusionWithWeights, LayerSimilarityClassifier
+from thesis.models.neural_net import AllLayerClassifier, GatedLayerFusion, MLP, LayerFusionWithWeights, LayerSimilarityClassifier,EnsembleLayerFusionWithWeights, LayerAtentionClassifier
 from thesis.models.lstm import LSTMModel
 from thesis.models.mamba import MambaClassifier
 
@@ -6,7 +6,7 @@ def get_model(cfg,embedding_size, num_layers):
     if cfg.model.name == "all_layer_classifier":
         return AllLayerClassifier(embedding_size=embedding_size, num_llm_layers=num_layers)
     elif cfg.model.name == "layer_fusion":
-        return LayerFusion(embedding_size=embedding_size, num_llm_layers=num_layers)
+        return GatedLayerFusion(embedding_size=embedding_size, num_llm_layers=num_layers)
     elif cfg.model.name == "lstm":
         return LSTMModel(embedding_size=embedding_size, num_llm_layers=num_layers,hidden_size=cfg.model.hidden_size,num_layers=cfg.model.num_layers)
     elif cfg.model.name == "mamba":
@@ -17,3 +17,7 @@ def get_model(cfg,embedding_size, num_layers):
         return LayerFusionWithWeights(embedding_size=embedding_size, num_llm_layers=num_layers)
     elif cfg.model.name == "layer_similarity_classifier":
         return LayerSimilarityClassifier(embedding_size=embedding_size, num_llm_layers=num_layers)
+    elif cfg.model.name == "ensemble_weight_fusion":
+        return EnsembleLayerFusionWithWeights(embedding_size=embedding_size, num_llm_layers=num_layers)
+    elif cfg.model.name == "layer_attention":  
+        return LayerAtentionClassifier(embedding_size=embedding_size, num_llm_layers=num_layers)
--- thesis/models/neural_net.py
+++ thesis/models/neural_net.py
@@ -52,16 +52,18 @@ class LRProbe(nn.Module):
 class AllLayerClassifier(nn.Module):
     def __init__(self, embedding_size, num_llm_layers):
         super().__init__()
-        
-        self.layer_classifiers = nn.Conv1d(num_llm_layers,num_llm_layers,embedding_size)
+        hidden_size = 100
+        self.token_encoder = nn.Linear(embedding_size,hidden_size)
+        self.layer_classifiers = nn.Conv1d(num_llm_layers,num_llm_layers,hidden_size)
         self.activation = nn.ReLU()
         self.aggregate = nn.Linear(num_llm_layers,1)
 
     def forward(self, x, return_encoded_space=False):
-        
-        result =  self.aggregate(self.activation(self.layer_classifiers(x)).squeeze(dim=-1))
+        encoded_space = self.activation(self.token_encoder(x))
+        result =  self.aggregate(self.activation(self.layer_classifiers(encoded_space)).squeeze(dim=-1))
         if return_encoded_space:
-            raise Exception("Contrastive Loss Not implemented")
+            #contrastive loss might not work
+            return result,encoded_space
         return result, None
     
     def plot_classifier_weights(self):
@@ -74,14 +76,16 @@ class AllLayerClassifier(nn.Module):
         wandb.log({"last_layer_weights":wandb.Image(fig)})
 
 
-class LayerFusion(nn.Module):
+class GatedLayerFusion(nn.Module):
     def __init__(self,num_llm_layers, embedding_size):
         super().__init__()
+        self.token_encoder = nn.Linear(embedding_size,100)
         self.gating = nn.Conv1d(num_llm_layers,num_llm_layers,embedding_size)
         self.activation = nn.ReLU()
         self.classifier = nn.Linear(embedding_size,1)
 
     def forward(self, x,return_encoded_space=False):
+        x = self.activation(self.token_encoder(x))
         weights = F.softmax(self.gating(x),dim=-2)
         encoded_space = torch.sum(weights*x,dim=1)
         result = self.classifier(encoded_space)
@@ -98,23 +102,17 @@ class LayerFusionWithWeights(nn.Module):
         super().__init__()
         #self.weights = nn.Parameter(torch.randn(num_llm_layers,embedding_size))
         self.weights = nn.Parameter(torch.randn(num_llm_layers,1))
-        #hidden_size = 100
-        #linear layer_layer #embbeding_size -> embedding_size for every num_llm_layers
-        #self.token_encoder = nn.Linear(embedding_size,hidden_size)
-        self.activation = nn.ReLU()
-        self.classifier = nn.Linear(embedding_size,1)
-        
-        """
-        kernel_size = embedding_size//100
-        hidden_size = (embedding_size - kernel_size + 2 * 0) // 1 + 1
-        self.token_encoder = nn.Conv1d(num_llm_layers,num_llm_layers,kernel_size)
+        hidden_size = 100
+        #linear layer_layer #embbeding_size -> embedding_size for every num_llm_layersnicolas alder
+        self.token_encoder = nn.Linear(embedding_size,hidden_size)
         self.activation = nn.ReLU()
         self.classifier = nn.Linear(hidden_size,1)
-        """
+        
     
     def forward(self, x,return_encoded_space=False):
         #x = [batch, num_layers, embedding_size]
-        #x = self.activation(self.token_encoder(x))
+        x = self.activation(self.token_encoder(x))
+        
         encoded_space = torch.sum(self.weights*x,dim=1)
         result = self.classifier(self.activation(encoded_space))
         if return_encoded_space:
@@ -125,53 +123,52 @@ class LayerFusionWithWeights(nn.Module):
     def plot_classifier_weights(self):
         weights = self.weights.data.squeeze()
         fig, ax = plt.subplots(figsize=(10, 5))
-        ax.bar(range(len(weights)), weights.cpu().numpy())
+        ax.bar(range(len(weights)), weights.cpu().numpy()) 
         ax.set_title('Layer Weights')
         ax.set_xlabel('Dimensions')
         ax.set_ylabel('Weight Value')
         wandb.log({"layer_weights":wandb.Image(fig)})         
         
-
-class LayerSimilarityClassifier(nn.Module):
-    #idea get layer_realtion with cosine_similarity
-    def __init__(self, num_llm_layers, embedding_size):
+class EnsembleLayerFusionWithWeights(nn.Module):
+    def __init__(self,num_llm_layers, embedding_size):
         super().__init__()
         
         self.num_llm_layers = num_llm_layers
         self.embedding_size = embedding_size
         
-        print("TODO: Still no hidden size explaination")       
+        self.weights = nn.Parameter(torch.randn(num_llm_layers,num_llm_layers,1))
         hidden_size = 100
+        #linear layer_layer #embbeding_size -> embedding_size for every num_llm_layers
         self.token_encoder = nn.Linear(embedding_size,hidden_size)
-        
-        self.similarity = nn.CosineSimilarity(dim=-1)
         self.activation = nn.ReLU()
-        self.final_classifier = nn.Linear(num_llm_layers*num_llm_layers,1)
+        self.classifier = nn.Linear(hidden_size,1)
+        self.aggregate = nn.Linear(num_llm_layers,1)
         
-
-    def forward(self, x, return_encoded_space=False):
-        #encode_x 
+    
+    def forward(self, x,return_encoded_space=False):
+        #x = [batch, num_layers, embedding_size
+        
+        #token encoder
         x = self.activation(self.token_encoder(x))
-               
-        #calculate layer_similarities
-        x_normalized = nn.functional.normalize(x, dim=-1)
-        similarities = torch.matmul(x_normalized, x_normalized.transpose(1, 2)).squeeze(-1) #-> [batch, num_layers, num_layers]
         
-        #classify similarities 
-        result = self.final_classifier(similarities.flatten(start_dim=1))
+        #weighted sum to encapsulate layer relation 
+        encoded_space = torch.sum(self.weights * x.unsqueeze(1), dim=2)
         
-        """ 
-        #--> for each layer independently 
-        hidden_layer = self.activation(self.layer_classifier(similarities)).squeeze(-1)
-        result = self.final_classifier(hidden_layer)
-        """
+             
         
+        #residual layer
+        #encoded_space = encoded_space + x 
+        
+        #classify and aggregate ensemble 
+        result = self.aggregate(self.activation(self.classifier(encoded_space)).squeeze(-1))
+            
         if return_encoded_space:
-            raise Exception("Contrastive Loss Not implemented")
+            return result,encoded_space
         return result, None
-
+    
+    
     def plot_classifier_weights(self):
-        weights = self.final_classifier.weight.data.squeeze(0).view(self.num_llm_layers,self.num_llm_layers)
+        weights = self.weights.data.view(self.num_llm_layers,self.num_llm_layers)
         
         # Create a figure for all layers with each layer having a subplot
         fig, axes = plt.subplots(self.num_llm_layers, 1, figsize=(10, 2 * self.num_llm_layers))
@@ -192,17 +189,137 @@ class LayerSimilarityClassifier(nn.Module):
         ax.set_ylabel('Weight Value')
         wandb.log({"sum_fusion_weights":wandb.Image(fig)})
         
+        aggregation_weights = self.aggregate.weight.data.squeeze(0)
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.bar(range(len(aggregation_weights)), aggregation_weights.cpu().numpy())
+        ax.set_title('Aggregation Weights')
+        ax.set_xlabel('Dimensions')
+        ax.set_ylabel('Weight Value')
+        wandb.log({"aggregation_weights":wandb.Image(fig)})
+        
+        
+        
+class LayerSimilarityClassifier(nn.Module):
+    #idea get layer_realtion with cosine_similarity
+    def __init__(self, num_llm_layers, embedding_size):
+        super().__init__()
+        
+        self.num_llm_layers = num_llm_layers
+        self.embedding_size = embedding_size
+        
+        print("TODO: Still no hidden size explaination")       
+        hidden_size = 100
+        self.token_encoder = nn.Linear(embedding_size,hidden_size)
+        
+        self.similarity = nn.CosineSimilarity(dim=-1)
+        self.activation = nn.ReLU()
+        self.final_classifier = nn.Linear(num_llm_layers*num_llm_layers,1)
+        
+
+    def forward(self, x, return_encoded_space=False):
+        #encode_x 
+        encoded_space = self.activation(self.token_encoder(x))
+               
+        #calculate layer_similarities
+        x_normalized = nn.functional.normalize(encoded_space, dim=-1)
+        similarities = torch.matmul(x_normalized, x_normalized.transpose(1, 2)).squeeze(-1) #-> [batch, num_layers, num_layers]
+        
+        #classify similarities 
+        result = self.final_classifier(similarities.flatten(start_dim=1))
+        
+        """ 
+        #--> for each layer independently 
+        hidden_layer = self.activation(self.layer_classifier(similarities)).squeeze(-1)
+        result = self.final_classifier(hidden_layer)
+        """
+        
+        if return_encoded_space:
+            #contrastive loss might not work
+            return result, encoded_space
+        return result, None
+
+    def plot_classifier_weights(self):
+        weights = self.final_classifier.weight.data.squeeze(0).view(self.num_llm_layers,self.num_llm_layers)
+        
+        # Create a figure for all layers with each layer having a subplot
+        fig, axes = plt.subplots(self.num_llm_layers, 1, figsize=(10, 2 * self.num_llm_layers))
+        for i in range(self.num_llm_layers):
+            axes[i].bar(range(self.num_llm_layers), weights[i].cpu().numpy())
+            axes[i].set_title(f'Layer {i} Weights')
+            axes[i].set_xlabel('Dimensions')
+            axes[i].set_ylabel('Weight Value')
+        plt.tight_layout()
+        wandb.log({"all_layers_weights":wandb.Image(fig)})
+
+        # Create a figure for the sum fusion of all layers
+        sum_weights = torch.abs(weights).sum(dim=0).cpu().numpy()
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.bar(range(self.num_llm_layers), sum_weights)
+        ax.set_title('Sum Fusion of All Layers Weights')
+        ax.set_xlabel('Dimensions')
+        ax.set_ylabel('Weight Value')
+        wandb.log({"sum_fusion_weights":wandb.Image(fig)})
+        
+
+class LayerAtentionClassifier(nn.Module):
+    
+    def __init__(self,num_llm_layers, embedding_size):
+        super().__init__()
+        
+        self.num_llm_layers = num_llm_layers
+        self.embedding_size = embedding_size
+        hidden_size = 100
+        
+        #model architecture
+        self.token_encoder = nn.Linear(embedding_size,hidden_size)
+        self.attention_fusion = torch.nn.MultiheadAttention(hidden_size, 1,batch_first=True)
+        self.activation = nn.ReLU()
+        self.classifier = nn.Linear(num_llm_layers,1)
+        self.aggregate = nn.Linear(num_llm_layers,1)
+        self.final_classifier = nn.Linear(num_llm_layers*num_llm_layers,1)
+        
+    
+    def forward(self, x,return_encoded_space=False):
+        #x = [batch, num_layers, embedding_size]
+        
+        #token encoder
+        x = self.activation(self.token_encoder(x))
+        
+        #attention fusion
+        encoded_space, weights = self.attention_fusion(x,x,x)
+        
+        #calculate layer_similarities
+        x_normalized = nn.functional.normalize(encoded_space, dim=-1)
+        similarities = torch.matmul(x_normalized, x_normalized.transpose(1, 2)).squeeze(-1) #-> [batch, num_layers, num_layers]
+        
+        #classify similarities 
+        result = self.final_classifier(similarities.flatten(start_dim=1))
+        
+        """ 
+        #--> for each layer independently 
+        hidden_layer = self.activation(self.layer_classifier(similarities)).squeeze(-1)
+        result = self.final_classifier(hidden_layer)
+        """
+        
+        if return_encoded_space:
+            return result, encoded_space
+        return result, None
+
+        
+        
+        
 
 if __name__ == "__main__":
     # Example usage
     embedding_size = 3584
     layer_size = 42
     batch_size = 100
-    model = LayerSimilarityClassifier(layer_size,embedding_size)
+    
+    model = LayerAtentionClassifier(layer_size,embedding_size)
 
     # Dummy input
     x = torch.randn(batch_size, layer_size, embedding_size)
     output, _ = model(x)
     print(output.shape)
     #print(output)
-    model.plotlayer_weights()
+    model.plot_classifier_weights()
