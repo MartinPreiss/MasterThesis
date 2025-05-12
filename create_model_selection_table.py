13e62ce558bb657df0ed1f7f9ad5aacb21845e30
MartinPreiss <martinpreiss524@gmail.com>
2025-05-12 11:11:36 +0200

    wip

--- create_model_selection_table.py
+++ create_model_selection_table.py
@@ -1,5 +1,7 @@
 import torch
 import os
+import pandas as pd
+import numpy as np
 
 def load_f1_scores_and_create_table(path,final_classifier, layer_depths, comparison_methods):
     # Initialize a dictionary to store F1 scores
@@ -38,22 +40,84 @@ def load_f1_scores_and_create_table(path,final_classifier, layer_depths, compari
 """
     return latex_table
 
+def print_statistics_of_classifier(path,final_classifier, layer_depths, comparison_methods):
+    # Initialize a dictionary to store F1 scores
+    f1_scores = {method: {depth: None for depth in layer_depths} for method in comparison_methods}
+
+    # Iterate over layer depths and comparison methods to load F1 scores
+    for layer_depth in layer_depths:
+        for comparison_method in comparison_methods:
+            file_name = f"layer_comparison_classifier_haluleval__1_{comparison_method}_{final_classifier}_{layer_depth}_False_5_f1s.pth"
+            file_path = os.path.join(path, file_name)
+            
+            if os.path.exists(file_path):
+                f1_scores[comparison_method][layer_depth] = torch.load(file_path).mean().item()
+            else:
+                #print(f"File not found: {file_path}")
+                continue
+    df = pd.DataFrame(f1_scores).dropna(axis=1)
+
+    print(f"{final_classifier}: mean={df.values.mean():.2f}, median={np.median(df.values):.2f}, std={df.values.std():.2f}, max={df.values.max():.2f}, min={df.values.min():.2f}")
+
+
+def print_statistics_of_layers(path,final_classifiers, layer_depths, comparison_methods):
+
+    # Iterate over layer depths and comparison methods to load F1 scores
+    for layer_depth in layer_depths:
+        f1_scores = {final_classifier: {method: None for method in comparison_methods} for final_classifier in final_classifiers}
+        for final_classifier in final_classifiers:
+            for comparison_method in comparison_methods:
+                file_name = f"layer_comparison_classifier_haluleval__1_{comparison_method}_{final_classifier}_{layer_depth}_False_5_f1s.pth"
+                file_path = os.path.join(path, file_name)
+                
+                if os.path.exists(file_path):
+                    f1_scores[final_classifier][comparison_method] = torch.load(file_path).mean().item()
+                else:
+                    #print(f"File not found: {file_path}")
+                    continue
+        df = pd.DataFrame(f1_scores).dropna(axis=1)
+        print(f"Layer depth {layer_depth}: mean={df.values.mean():.2f}, median={np.median(df.values):.2f}, std={df.values.std():.2f}, max={df.values.max():.2f}, min={df.values.min():.2f}")
+
+def print_statistics_of_comparison_methods(path,final_classifiers, layer_depths, comparison_methods):
+
+    for comparison_method in comparison_methods:
+        f1_scores = {final_classifier: {depth: None for depth in layer_depths} for final_classifier in final_classifiers}
+        for final_classifier in final_classifiers:
+            for layer_depth in layer_depths:
+                file_name = f"layer_comparison_classifier_haluleval__1_{comparison_method}_{final_classifier}_{layer_depth}_False_5_f1s.pth"
+                file_path = os.path.join(path, file_name)
+                
+                if os.path.exists(file_path):
+                    f1_scores[final_classifier][layer_depth] = torch.load(file_path).mean().item()
+                else:
+                    #print(f"File not found: {file_path}")
+                    continue
+        df = pd.DataFrame(f1_scores).dropna(axis=1)
+        print(f"Comparison method {comparison_method}: mean={df.values.mean():.2f}, median={np.median(df.values):.2f}, std={df.values.std():.2f}, max={df.values.max():.2f}, min={df.values.min():.2f}")
 
 if __name__ == "__main__":
     path = "./thesis/data/avgs_early_stopping"
-    layer_depths = [0, 1, 2, 3, 4, 5]
+    layer_depths = [1,2]
     comparison_methods = [
         "no_comparison", "dot_product", "euclidean_norm", "manhatten",
         "pairwise_dot_product", "euclidean_distance", "manhatten_distance", "cosine"
     ]
 
-    aggregation_methods=["shared_classifier_ensemble","flattend_aggregation"]
-    linearity_classifier=[True, False]
+    #aggregation_methods=["shared_classifier_ensemble","flattend_aggregation"]
+    aggregation_methods = ["flattend_aggregation"]
+    linearity_classifier=[True,False]
 
 
     final_layer_classifiers = [method + "_" + str(linearity) for method in aggregation_methods for linearity in linearity_classifier]
     
     for final_classifier in final_layer_classifiers:
-        latex_table = load_f1_scores_and_create_table(path,final_classifier=final_classifier,layer_depths=layer_depths,comparison_methods=comparison_methods)
-        print(final_classifier)
-        print(latex_table)
+        #latex_table = load_f1_scores_and_create_table(path,final_classifier=final_classifier,layer_depths=layer_depths,comparison_methods=comparison_methods)
+        #print(final_classifier)
+        #print(latex_table)
+        print_statistics_of_classifier(path,final_classifier=final_classifier,layer_depths=layer_depths,comparison_methods=comparison_methods)
+
+    print("Statistics of layers")
+    print_statistics_of_layers(path,final_classifiers=final_layer_classifiers,layer_depths=layer_depths,comparison_methods=comparison_methods)
+
+    print("Statistics of comparison methods")
+    print_statistics_of_comparison_methods(path,final_classifiers=final_layer_classifiers,layer_depths=layer_depths,comparison_methods=comparison_methods)
